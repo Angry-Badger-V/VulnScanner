147fa6b7a803ba5602ee91dae1201f60fe79c33f
Angry-Badger-V <86304067+Angry-Badger-V@users.noreply.github.com>
2025-07-17 11:38:03 +0100

    Rename and begin html parse logic

--- src/scanners/sqli-scan.py
+++ src/scanners/sqli-scan.py
@@ -1,18 +1,31 @@
 # sqli-scan.py
 import requests
 from bs4 import BeautifulSoup as soup
-from __scaner_state import ScannerState
+from __scanner_state import ScannerState
 
 name = "sqli-scan"
 description = "Detects SQLI attacks on target"
 
-def parse_target(target_html):
-    pass
+def parse_target(target_html, state):
+    parsed = soup(target_html.text, "html.parser")
+
+    # Extract all elements that might contain SQLI vulnerabilities
+    for url in parsed.find_all("a", href=True):
+        url_info = {
+            "href": url.get("href"), 
+            "text": url.text.strip()
+        }
+        state.add_url(url)
+
+    #
+    # Do same for forms, cookies, headers
+    #
 
 def run(target, report):
     state = ScannerState()
 
-    target_html = requests.get(target)
+    target_html = requests.get(target, state)
+
 
 
 
